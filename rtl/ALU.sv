0afa7ed58ec5478075a74287ff098920ce0373f5
Niklas Thomas <niktho@kth.se>
2025-06-30 20:30:54 +0200

    Implemented ALU and verified it

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -2,9 +2,55 @@ module ALU #(parameter REG_BITS=32)
 (
     input logic [REG_BITS-1:0] A, B,
     input logic [3:0] ctrl,
-    output logic [2:0] ONZ,
+    output logic [3:0] ONZC,
     output logic [REG_BITS-1:0] C 
 );
+
+   logic [REG_BITS:0] tmpResult;
+
+   always_comb begin
+       ONZC = 4'b0000;
+       tmpResult = 0;
+       case(ctrl)
+           4'b0000: begin //add
+                tmpResult = A + B;
+                C = tmpResult[REG_BITS-1:0];
+                ONZC[0] = tmpResult[REG_BITS];
+                if (C == 0) ONZC[1] = 1;
+                if (C[REG_BITS-1] == 1) ONZC[2] = 1; 
+                if ((A[REG_BITS-1] == B[REG_BITS-1]) && (A[REG_BITS-1] != C[REG_BITS-1])) ONZC[3] = 1;
+           end
+           4'b0001:begin  //sub
+                tmpResult = A - B;
+                C = tmpResult[REG_BITS-1:0];
+                ONZC[0] = tmpResult[REG_BITS];
+                if (C == 0) ONZC[1] = 1;
+                if (C[REG_BITS-1] == 1) ONZC[2] = 1; 
+                if ((A[REG_BITS-1] == B[REG_BITS-1]) && (A[REG_BITS-1] != C[REG_BITS-1])) ONZC[3] = 1;
+           end
+           4'b0010: //and 
+                C = A & B;
+           4'b0011: //or
+                C = A | B;
+           4'b0100: //xor
+                C = A ^ B;
+           4'b0101: //slt
+                if ($signed(A) < $signed(B)) C = 1;
+                else C = 0;
+           4'b0110: begin  //sll
+                C = A << B[4:0];
+           end
+           4'b0111: //sltu
+               if ($unsigned(A) < $unsigned(B)) C = 1;
+               else C = 0;
+           4'b1000: //srl
+                C = A >> B[4:0];
+           4'b1001: //sra
+                C = $signed(A) >>> B[4:0];
+           default:
+                C = 'b0;
+       endcase
+   end
 
 endmodule
 
---
+++ tb/ALU_tb.sv
@@ -0,0 +1,84 @@
+module ALU_tb #(parameter REG_BITS=5)
+(
+    output logic [3:0] ONZC,
+    output logic [REG_BITS-1:0] C
+);
+    
+    logic [REG_BITS-1:0] A,B;
+    logic [3:0] ctrl;
+
+   ALU #(.REG_BITS(REG_BITS)) dut (.A(A), .B(B), .C(C), .ctrl(ctrl), .ONZC(ONZC));
+
+   initial begin
+       $dumpfile("ALU_tb.vcd");
+       $dumpvars(0, ALU_tb);
+       A = 0;
+       B = 0;
+       ctrl = 4'b0000; // add
+
+       #5ns;
+
+       A = 5;
+       B = 5;
+
+       #5ns;
+
+       A = 5'b11110;
+       B = 5'b00001;
+
+       #5ns;
+
+       B = 5'b00010;
+
+       #5ns;
+
+       A = 5'b01111;
+
+       #5ns;
+
+       ctrl = 4'b0001; // sub
+
+       #5ns;
+
+       ctrl = 4'b0010; // and
+
+       #5ns;
+
+       B = 5'b00001;
+       ctrl = 4'b0011; // or
+
+       #5ns;
+
+       ctrl = 4'b0101; // slt
+       A = 5'b00000;
+       B = 5'b00001;
+
+       #5ns;
+
+       B = 5'b11111;
+
+       #5ns;
+
+       A = 5'b11010;
+       B = 5'b00001;
+       ctrl = 4'b0110; //sll
+
+       #5ns;
+
+       ctrl = 4'b0111; // sltu
+
+       #5ns;
+
+       ctrl = 4'b1000; // srl
+
+       #5ns;
+
+       A = 5'b10000;
+       ctrl = 4'b1001; // sra
+
+       #5ns;
+
+   end
+
+endmodule
+
