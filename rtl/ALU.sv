6edcbc36b2741792213aed6d2be23aa368291d0a
Niklas Thomas <niktho@kth.se>
2025-07-01 21:37:50 +0200

    Updated ALU and verified it

--- rtl/ALU.sv
+++ rtl/ALU.sv
@@ -7,10 +7,12 @@ module ALU #(parameter REG_BITS=32)
 );
 
    logic [REG_BITS:0] tmpResult;
+   logic [REG_BITS:0] tmp;
 
    always_comb begin
        ONZC = 4'b0000;
        tmpResult = 0;
+       tmp = 0;
        case(ctrl)
            4'b0000: begin //add
                 tmpResult = A + B;
@@ -24,7 +26,6 @@ module ALU #(parameter REG_BITS=32)
                 tmpResult = A - B;
                 C = tmpResult[REG_BITS-1:0];
                 ONZC[0] = tmpResult[REG_BITS];
-                if (C == 0) ONZC[1] = 1;
                 if (C[REG_BITS-1] == 1) ONZC[2] = 1; 
                 if ((A[REG_BITS-1] == B[REG_BITS-1]) && (A[REG_BITS-1] != C[REG_BITS-1])) ONZC[3] = 1;
            end
@@ -34,22 +35,33 @@ module ALU #(parameter REG_BITS=32)
                 C = A | B;
            4'b0100: //xor
                 C = A ^ B;
-           4'b0101: //slt
+           4'b0101: begin//slt
                 if ($signed(A) < $signed(B)) C = 1;
                 else C = 0;
-           4'b0110: begin  //sll
-                C = A << B[4:0];
            end
-           4'b0111: //sltu
+           4'b0110: begin  //sll
+                tmp = {1'b0, A} << B[4:0];
+                C = tmp[REG_BITS-1:0];
+                ONZC[0] = tmp[REG_BITS];
+           end
+           4'b0111: begin//sltu
                if ($unsigned(A) < $unsigned(B)) C = 1;
                else C = 0;
-           4'b1000: //srl
-                C = A >> B[4:0];
-           4'b1001: //sra
-                C = $signed(A) >>> B[4:0];
+           end
+           4'b1000: begin//srl
+                tmp = {A, 1'b0} >> B[4:0];
+                C = tmp[REG_BITS:1];
+                ONZC[0] = tmp[0];
+           end
+           4'b1001: begin //sra
+                tmp = $signed({A, 1'b0}) >>> B[4:0];
+                C = tmp[REG_BITS:1];
+                ONZC[0] = tmp[0];
+           end
            default:
                 C = 'b0;
        endcase
+       if (C == 0) ONZC[1] = 1;
    end
 
 endmodule
