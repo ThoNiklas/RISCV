8bb7adc9e9940e2565fa57a7397882a2da67e445
Niklas Thomas <niktho@kth.se>
2025-07-04 11:27:02 +0200

    Implemented Instruction Memory

--- rtl/InstructionMemory.sv
+++ rtl/InstructionMemory.sv
@@ -1,12 +1,20 @@
+/* verilator lint_off UNUSEDSIGNAL */
+
 module InstructionMemory #(parameter REG_BITS=32) 
 (
-    input logic  [REG_BITS-1:0] a,
-    output logic  [REG_BITS-1:0] rd
+    input logic  [REG_BITS-1:0] A,
+    output logic  [REG_BITS-1:0] RD
 );
 
-    reg [REG_BITS-1:0] mem [0 : 2 ** REG_BITS - 1] = '{default:0};
-    assign mem[1] = 32'b1;
+    reg [REG_BITS-1:0] mem [0 : 2 ** 15 - 1];
+    initial begin
+        $display("Read Rom");
+        $readmemh("./mem/hex_rom.mem", mem); // load hex file
+        //$readmemb("../mem/bin_rom.mem", mem); // load bin file
 
-    assign rd = mem[a];
+    end
+
+    assign RD = mem[A];
+
 endmodule
 
