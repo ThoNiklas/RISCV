46e31951dc489788902eba06ceea466a6063a5a6
Niklas Thomas <niktho@kth.se>
2025-07-10 18:28:00 +0200

    Added two example assembler codes used for verification

---
+++ code/load_store.asm
@@ -0,0 +1,13 @@
+        addi  r1, r0, -1
+        sb    r1, 0(r0)       
+        addi  r1, r0, -2
+        sh    r1, 4(r0)
+        addi  r1, r0, -4
+        sw    r1, 8(r0)
+        lb    r2, 0(r0)
+        lh    r3, 4(r0)
+        lw    r4, 8(r0)
+        lbu    r2, 0(r0)
+        lhu    r3, 4(r0)
+
+
